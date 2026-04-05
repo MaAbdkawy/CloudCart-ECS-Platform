53600bd97bdf9bf869c73d7fd22d90e8f4fbe2e6
MaAbdkawy <144789985+MaAbdkawy@users.noreply.github.com>
2026-04-05 08:47:45 +0200

    Delete ECS-repo directory

--- ECS-repo/dbinit/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "MYSQL_HOST=$MYSQL_HOST"
-echo "Running DB init..."
-
-mysql --disable-ssl -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /init.sql
-
-echo "DB init completed successfully"
